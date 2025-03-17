18ff99897dd9f08bf1f03b3d2526245924f73143
2904760 <2904760g@student.gla.ac.uk>
2025-03-17 22:26:55 +0530

    Added MVC files for user authentication and changes for Signup along with JWT

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,66 @@
+import { generateTokens } from "../lib/utils.js"
+import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
+
 export async function handleUserSignUp(req,res) {
-    res.send("Sign up Page")
+    const {fullName, email, password} = req.body
+
+    try{
+        // Check if all fields have been filled
+        if(!fullName || !email || !password){
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+        }
+        // Check password validity
+        if(password.length < 6){
+            return res.status(400).json({
+                message: "Password must be atleast 6 characters"
+            })
+        }
+
+        // Check if user exists
+        const user = await User.findOne({email})
+        if (user){
+            return res.status(400).json({
+                message: "Email already exists"
+            })
+        }
+
+        // Hash Password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        // Create User
+        const newUser = new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword,
+        })
+
+        if(newUser){
+            // generate jwt
+            generateTokens(newUser._id, res)
+            await newUser.save() 
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
+        }else{
+            return res.status(400).json({
+                message: "Invalid user data"
+            })
+        }
+
+    }catch(error){
+        console.log("Error in Signup controller", error.message)
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
 }
 
 export async function handleUserLogin(req,res) {
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+export const generateTokens = (userId, res) => {
+    const token = jwt.sign(
+        {userId},
+        process.env.JWT_SECRET,
+        {expiresIn:"7d"}
+    )
+
+    res.cookie("jwt",token,{
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // prevent XSS attacks, cross-site scripting attacks
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development" // For http or https
+    })
+
+    return token
+}
