c4063452f77878fc023601a14d3be05bffebcbb6
2904760 <2904760g@student.gla.ac.uk>
2025-03-29 13:45:48 +0530

    Changes for setting page and themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,14 @@ import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import Navbar from "./components/Navbar"
 import { useAuthStore } from './store/useAuthStore'
+import { useThemeStore } from './store/useThemeStore'
 import { useEffect } from 'react'
 
 const App = () => {
   const{authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const{ theme } = useThemeStore()
 
+  
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
@@ -29,13 +32,13 @@ if(isCheckingAuth && !authUser){
 }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage />: <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage />: <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage />: <Navigate to="/" />} />
-        <Route path="/settings" element={!authUser ? <SettingsPage />: <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
